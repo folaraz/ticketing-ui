13be207fe2752f572ac689243a6279382d82178f
Olajide Abdulrazzaq Folarin <folaraz11@gmail.com>
2021-08-02 20:47:41 +0100

    added routes

--- src/components/homepage/HomePage.js
+++ src/components/homepage/HomePage.js
@@ -1,40 +1,14 @@
 import React, {Component} from 'react';
 import Login from "./login/Login";
-import SignUp from "./signup/SignUp";
 
 class HomePage extends Component {
 
-    state = {
-        loadLoginPage: false,
-        loadSignUpPage: false
-    }
-
-    handleLogin = () => {
-        this.setState({loadLoginPage: true})
-
-    }
-
-    handleSignUp = () => {
-        this.setState({loadSignUpPage: true})
-    }
-
     render() {
-        const {loadLoginPage, loadSignUpPage} = this.state;
-        let pageToRender = <div></div>;
-        if (loadLoginPage) {
-            pageToRender = <Login/>
-        }
-        if (loadSignUpPage) {
-            pageToRender = <SignUp/>
-        }
-        return (<div>
-            {loadLoginPage || loadSignUpPage === true ? pageToRender :
-                <div>
-                    <button className="btn success" onClick={this.handleSignUp}>Sign Up</button>
-                    <button className="btn success" onClick={this.handleLogin}>Login</button>
-                </div>
-            }
-        </div>)
+        return (
+            <div>
+                Homepage
+            </div>
+        )
     }
 
 }
