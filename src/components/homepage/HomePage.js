cc6fc8bffe9f620728a80d5f3d7ae7872c13c550
folaneem <folaneem@gmail.com>
2021-08-22 19:44:55 +0100

    addressed review changes

--- src/components/homepage/HomePage.js
+++ src/components/homepage/HomePage.js
@@ -17,32 +17,33 @@ class HomePage extends Component {
                         <ul>
                             <li><a href={'#'}>About</a></li>
                             <li><a href={'#'}>Pricing</a></li>
-                            <li><a href={'#'}>Contact</a></li></ul>
+                            <li><a href={'#'}>Contact</a></li>
+                        </ul>
 
-<ul>
-               <Link to="/login">
-                <button   className='btn-submit-form' onClick="/login">Log In</button>
-                </Link>
-                <Link to="/sign-up">
-                    <button className='btn-submit-form' onClick="/signup">Sign Up</button>
-                </Link>
+                        <ul>
+                            <Link to="/login">
+                                <button className='btn-submit-form' onClick="/login">Log In</button>
+                            </Link>
+                            <Link to="/sign-up">
+                                <button className='btn-submit-form' onClick="/signup">Sign Up</button>
+                            </Link>
                         </ul>
                     </div>
                 </div>
 
-<div className={'content'}>
-    <div className={'textBox'}>
-        <h2> Get Your Car Parked Safely with <span> Given name</span></h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-        <a href={'#'}>Learn More</a>
-    </div>
+                <div className={'content'}>
+                    <div className={'textBox'}>
+                        <h2> Get Your Car Parked Safely with <span> Given name</span></h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        <a href={'#'}>Learn More</a>
+                    </div>
 
 
-</div>
+                </div>
 
             </div>
         )
