0cd5b84ee1b0290962de4bc12075bb6bdef45b42
folaneem <folaneem@gmail.com>
2021-08-22 16:31:51 +0100

    addressed review changes

--- src/components/homepage/HomePage.js
+++ src/components/homepage/HomePage.js
@@ -7,40 +7,47 @@ class HomePage extends Component {
     render() {
         return (
             <div className={'Homepage'}>
+                <div className={'circle'}>
+
+                </div>
 
                 <div className={'Homepage__header'}>
-                    <span className={'title'}>Ultimate</span>
-                    <div className="dropdown">
-                        <Link className="dropbtn">features
-                        </Link>
-                        <div className="dropdown-content">
-                            <p>We have amazing parking spaces that can take over a hundred vehicles,also different parking spaces for car,bicycle and lorry.</p>
-                        </div>
-                    </div>
+
                     <div className={'Header__nav'}>
+                        <ul>
+                            <li><a href={'#'}>About</a></li>
+                            <li><a href={'#'}>Pricing</a></li>
+                            <li><a href={'#'}>Contact</a></li>
 
-               <tr> <td><Link to="/login">
-                <button   id='btn-submit-form-one' onClick="/login">Log In</button>
+                        </ul>
+               <Link to="/login">
+                <button   className='btn-submit-form' onClick="/login">Log In</button>
                 </Link>
-               </td>
-              <td> <Link to="/sign-up">
-                    <button id={'btn-submit-form-two'} onClick="/signup">Sign Up</button>
+                <Link to="/sign-up">
+                    <button className='btn-submit-form' onClick="/signup">Sign Up</button>
                 </Link>
-              </td>
-               </tr>
                     </div>
                 </div>
-                <div className={'Homepage__container'}>
-                    <img className="home__image" src="./images/body.jpg" alt=""/>
-                    <div className={'container__one'}>
-                        <h1>Get Your Vehicle <br/>Parked</h1>
-                        <p>With condusive parking spaces,an app,tools&guides <br/> Ultimate is the best way to safeguard your vehicle</p>
- <br/>
-                        <button  className={'btn-submit-form-three'} onClick='#'> Get it on <br/> <img  src={"./images/playstore.png"} width={'22px'}/>  <strong>Play Store</strong> </button>
-                        <button  className={'btn-submit-form-three'} onClick='#'>   Get it on <br/>  <img  src={"./images/apple.png"} width={'25px'}/> <strong>App Store</strong> </button>
-                    </div>
 
-                </div>
+<div className={'content'}>
+    <div className={'textBox'}>
+        <h2> Get Your Car Parked Safely with <span> Given name</span></h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        <a href={'#'}>Learn More</a>
+    </div>
+
+
+</div>
+                <p>You can visit us on</p>
+                <ul className={'sci'}>
+                    <li><a href={'#'}><img src={'./images/facebook.png'}/></a></li>
+                    <li><a href={'#'}><img src={'./images/twitter.png'}/></a></li>
+                    <li><a href={'#'}><img src={'./images/instagram.png'}/></a></li>
+                </ul>
             </div>
         )
     }
